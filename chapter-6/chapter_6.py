b022e781561a82aa37d681d62c9f7dfd8e45f23d
Breanne Boland <breanneboland@gmail.com>
2017-02-06 23:27:06 -0800

    finished chapter six

--- chapter-6/chapter_6.py
+++ chapter-6/chapter_6.py
@@ -17,6 +17,7 @@ word4 = ""
 # middle(word1)
 # first(word4)
 
+# Exercise 6-6
 def is_palindrome(word):
     if len(word) <= 1:
         return True
@@ -24,7 +25,21 @@ def is_palindrome(word):
         return False
     return is_palindrome(middle(word))
 
-print(is_palindrome("allen"))
-print(is_palindrome("bob"))
-print(is_palindrome("a man a plan a canal panama"))
-print(is_palindrome("p"))
+# print(is_palindrome("allen"))
+# print(is_palindrome("bob"))
+# print(is_palindrome("a man a plan a canal panama"))
+# print(is_palindrome("p"))
+
+# Exercise 6-7
+def is_power(a, b):
+    if type(a) != int and type(b) != int:
+        print("Not ints, can't play.")
+    elif a % b == 0 and (a/b) % b == 0:
+        print(True)
+    else:
+        print(False)
+
+is_power(4, 2)
+is_power(16, 4)
+is_power(23, 5)
+is_power("donut", 1.5)
